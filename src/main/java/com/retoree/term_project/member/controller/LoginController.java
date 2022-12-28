faff539ed77b868993c35d191fbb7ecfe0cfbb0e
seonbuzz <ddonsdevelop@gmail.com>
2022-12-28 15:19:26 +0900

    login commit

---
+++ src/main/java/com/retoree/term_project/member/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.retoree.term_project.member.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/login.me")
+public class LoginController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}

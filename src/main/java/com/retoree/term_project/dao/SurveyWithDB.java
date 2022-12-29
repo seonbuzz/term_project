59725f3ae36d12ac1faff40e88e8c58f1cfe75fc
mayds1010 <mayds1010@gmail.com>
2022-12-29 10:55:01 +0900

    surveycommit

--- src/main/java/com/retoree/term_project/dao/SurveyWithDB.java
+++ src/main/java/com/retoree/term_project/dao/SurveyWithDB.java
@@ -3,18 +3,19 @@ package com.retoree.term_project.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SurveyWithDB{
-    private static String questionsUid; //추가됨
+     //추가됨
 
-    public static HashMap<String, Object> getQuestion(String questionUid) throws SQLException{ 
+    public HashMap<String, Object> getQuestion(String questionUid) throws SQLException{ 
         
         Commons commons = new Commons();
         Statement statement = commons.getStatement();
  
     String query = "SELECT * FROM QUESTIONS_LIST " +
-      " WHERE QUESTIONS_UID = '"+questionsUid+"'";
+      " WHERE QUESTIONS_UID = '"+questionUid+"'";
 
       ResultSet resultSet = statement.executeQuery(query);
       HashMap<String, Object> result = null;
@@ -26,4 +27,28 @@ public class SurveyWithDB{
       }
       return result;
     } 
+    public ArrayList<HashMap>getAnswerList(String quesionsUid) throws SQLException{
+      Commons commons = new Commons();
+      Statement statement = commons.getStatement();
+      String query = "SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE " +
+                     "FROM ANSWERS "+
+                     "INNER JOIN EXAMPLE_LIST "+
+                     "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID "+
+                     "WHERE QUESTIONS_UID = '"+quesionsUid+"' " +
+                     "ORDER BY QUESTIONS_UID ";
+      ResultSet resultSet = statement.executeQuery(query);
+
+      ArrayList<HashMap> answer_list = new ArrayList<>();
+      while(resultSet.next()){
+        HashMap<String,Object> answer = new HashMap<String,Object>();
+        answer.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
+        answer.put("EXAMPLE", resultSet.getString("EXAMPLE"));
+        answer.put("ORDERS", resultSet.getInt("ORDERS"));
+     
+        answer_list.add(answer);
+      }
+
+      return answer_list;
+      
+    }
 }
--- src/main/java/com/retoree/term_project/servlets/SurveyServlet.java
+++ src/main/java/com/retoree/term_project/servlets/SurveyServlet.java
@@ -1,6 +1,7 @@
 package com.retoree.term_project.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.retoree.term_project.dao.SurveyWithDB;
@@ -14,22 +15,35 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "") //아직 작성x
 public class SurveyServlet extends HttpServlet{
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        // input type
        String questions_Uid = request.getParameter("QUESTIONS_UID");
 
        SurveyWithDB surveyWithDB = new SurveyWithDB();
-       HashMap<String, Object> quetion = null;
+       HashMap<String, Object> question = null;
+       ArrayList<HashMap> answer_list = null;
        try {
-        quetion = SurveyWithDB.getQuestion(questions_Uid);
-        
-       } catch (Exception e) {
-       
-       }
-        
+        question = surveyWithDB.getQuestion(questions_Uid);
 
-       RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/details.jsp");
+        System.out.println(question.get("QUESTIONS_UID"));
+        System.out.println(question.get("QUESTIONS"));
+        System.out.println(question.get("ORDERS"));
+       
+        answer_list = surveyWithDB.getAnswerList(questions_Uid);
+       } catch (Exception e) {
+        e.printStackTrace();
+       }
+        for(int i = 0; i< answer_list.size(); i++){
+            HashMap<String,Object> answer = answer_list.get(i);
+            System.out.println(answer.get("ORDERS"));
+            System.out.println(answer.get("EXAMPLE"));
+        }
+        request.setAttribute("question", question);
+        request.setAttribute("answer_list", answer_list);
+
+       RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/survey.jsp");
        requestDispatcher.forward(request, response);
     }
     

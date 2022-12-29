86728743ce1d06216ad9f6ffb68f9a6b9c210c7c
seonbuzz <ddonsdevelop@gmail.com>
2022-12-29 13:19:37 +0900

    login

--- src/main/java/com/retoree/term_project/member/controller/LoginController.java
+++ src/main/java/com/retoree/term_project/member/controller/LoginController.java
@@ -8,23 +8,24 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/login.me")
+@WebServlet(urlPatterns = "/login.me")
 public class LoginController extends HttpServlet {
 
-
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
-        String userId = request.getParameter("userId");
-        String userPwd = request.getParameter("userPwd");
-        
+        String id = request.getParameter("id");
+        String pwd = request.getParameter("pwd");
+
         // new MemberBean().loginMember(userId, userPwd);
         // System.out.println();
     }
-    
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         this.doGet(request, response);
     }
 }

985236d08a2a16e86d15557fc9b761d05c03f274
mayds1010 <mayds1010@gmail.com>
2022-12-30 17:20:33 +0900

    commit

--- src/main/java/com/retoree/term_project/dao/SurveyWithDB.java
+++ src/main/java/com/retoree/term_project/dao/SurveyWithDB.java
@@ -10,8 +10,6 @@ import com.mysql.cj.protocol.Resultset;
 
 public class SurveyWithDB{
 
-
-    // public ArrayList<HashMap<String, String>> getQuestions(String questions_Uid) throws SQLException{
     public ArrayList<HashMap<String, String>> getQuestions(){
 
       Commons commons = new Commons();
@@ -137,51 +135,6 @@ public class SurveyWithDB{
 
 
 
-//전에꺼
-  //  public HashMap<String, Object> getQuestion(String questions_Uid) throws SQLException{ //quesionsUid
-        
-  //       Commons commons = new Commons();
-  //       Statement statement = commons.getStatement();
- 
-  //   String query = "SELECT * FROM QUESTIONS_LIST " +
-  //     " WHERE QUESTIONS_UID = '"+questions_Uid+"'";
-
-  //     ResultSet resultSet = statement.executeQuery(query);
-  //     HashMap<String, Object> result = null;
-  //     while(resultSet.next()){
-  //       result = new HashMap<>();
-  //       result.put("QUESTIONS_UID",resultSet.getString("QUESTIONS_UID"));
-  //       result.put("QUESTIONS",resultSet.getString("QUESTIONS"));
-  //       result.put("ORDERS",resultSet.getInt("ORDERS"));
-  //     }
-  //     return result; 
-  //   } 
-  //   public ArrayList<HashMap>getAnswersList(String questions_Uid) throws SQLException{ //quesionsUid
-  //     Commons commons = new Commons();
-  //     Statement statement = commons.getStatement();
-  //     String query = "SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE " +
-  //                    "FROM ANSWERS "+
-  //                    "INNER JOIN EXAMPLE_LIST "+
-  //                    "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID "+
-  //                    "WHERE QUESTIONS_UID = '"+questions_Uid+"' " +
-  //                    "ORDER BY QUESTIONS_UID ";
-  //     ResultSet resultSet = statement.executeQuery(query);
-
-  //     ArrayList<HashMap> answers_list = new ArrayList<>();
-  //     while(resultSet.next()){
-  //       HashMap<String,Object> answers = new HashMap<String,Object>();
-  //       answers.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
-  //       answers.put("EXAMPLE", resultSet.getString("EXAMPLE"));
-  //       answers.put("ORDERS", resultSet.getInt("ORDERS"));
-     
-  //       answers_list.add(answers);
-  //     }
-
-  //     return answers_list;
-      
-  //   }
-
-       
 
 
 

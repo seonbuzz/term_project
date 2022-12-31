e06d12aaf7b73ba273f30915fef9b7a7d6cc3853
mayds1010 <mayds1010@gmail.com>
2022-12-31 16:27:41 +0900

    surveycommit

--- src/main/java/com/retoree/term_project/dao/SurveyWithDB.java
+++ src/main/java/com/retoree/term_project/dao/SurveyWithDB.java
@@ -52,8 +52,8 @@ public class SurveyWithDB{
                          "FROM ANSWERS "+
                          "INNER JOIN EXAMPLE_LIST "+
                          "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID "+
-                         "WHERE QUESTIONS_UID = '"+ QUESTIONS_UID +"' " +
-                         "ORDER BY QUESTIONS_UID ";
+                         "WHERE QUESTIONS_UID = '"+ QUESTIONS_UID +"' " + 
+                         "ORDER BY QUESTIONS_UID "; 
        
 
       ArrayList<HashMap<String, String>> answers = new ArrayList<>();
@@ -74,61 +74,7 @@ public class SurveyWithDB{
       }
    }
 
-//다시한거
-// public ArrayList<HashMap<String, String>> getQuestions(){
 
-//   Commons commons = new Commons();
-//   Statement statement = commons.getStatement();
-
-//   String query = "SELECT * FROM QUESTIONS_LIST ORDER BY ORDERS;";
-
-  
-//   ArrayList<HashMap<String, String>> questions = new ArrayList<>();
-//   try {
-//     ResultSet resultSet = statement.executeQuery(query);
-//     while(resultSet.next()){
-//       HashMap<String, String> question = new HashMap<>();
-//       question.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
-//       question.put("QUESTIONS", resultSet.getString("QUESTIONS"));
-//       question.put("ORDERS", resultSet.getString("ORDERS"));
-
-//       questions.add(question);
-//   }
-//   } catch (SQLException e) {
-//     e.getStackTrace();
-//   }
-  
-//   return questions;
-// }
-
-// public ArrayList<HashMap<String, String>> getAnswersList(){
-
-//   Commons commons = new Commons();
-//   Statement statement = commons.getStatement();
-
-//   String query = "SELECT ANSWERS.QUESTIONS_UID, EXAMPLE_LIST.ORDERS, EXAMPLE_LIST.EXAMPLE " +
-//                      "FROM ANSWERS "+
-//                      "INNER JOIN EXAMPLE_LIST "+
-//                      "ON ANSWERS.EXAMPLE_UID = EXAMPLE_LIST.EXAMPLE_UID "+
-//                      "WHERE QUESTIONS_UID = '"+ questions_Uid+"' " +
-//                      "ORDER BY QUESTIONS_UID ";
-//   ArrayList<HashMap<String, String>> answers = new ArrayList<>();
-//   try {
-//     ResultSet resultSet = statement.executeQuery(query);
-//     while(resultSet.next()){
-//       HashMap<String, String> answer = new HashMap<>();
-//       answer.put("QUESTIONS_UID", resultSet.getString("QUESTIONS_UID"));
-//       answer.put("ORDERS", resultSet.getString("ORDERS"));
-//       answer.put("EXAMPLE", resultSet.getString("EXAMPLE"));
-
-//       answers.add(answer);
-//     }
-//       } catch (Exception e) {
-//             e.getStackTrace(); 
-//   }                   
-//      return answers;
-//   }
-// }
 
 
 

24ee8c01b8df0bca37711b1cc445d6a37549f996
byeongjuPark <bottlepark28@gmail.com>
2023-01-02 15:25:22 +0900

    1

--- src/main/java/com/retoree/term_project/servlets/StatisticsServeyServlet.java
+++ src/main/java/com/retoree/term_project/servlets/StatisticsServeyServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.retoree.term_project.Bean.Member;
 import com.retoree.term_project.dao.StatisticsWithDB;
 
 import jakarta.servlet.RequestDispatcher;
@@ -12,23 +13,34 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns="/statisticsServeyServlet")
 public class StatisticsServeyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         StatisticsWithDB db = new StatisticsWithDB();
+        HttpSession httpSession = request.getSession();
+        Member loginUser = (Member)httpSession.getAttribute("loginUser");
         HashMap<String, String> statistics = new HashMap<>();
+        RequestDispatcher requestDispatcher = null;
         // String checkUid="";
 
-        statistics = db.getStatisticsSurvey();
-        System.out.println(statistics);
+        if (loginUser == null){
+            requestDispatcher = request.getRequestDispatcher("/views/errorPage3.jsp");
+            requestDispatcher.forward(request, response);
 
+        }else{
+            if(loginUser.getAuth().equals("YES")){
+                requestDispatcher = request.getRequestDispatcher("/views/result3.jsp");
+                statistics = db.getStatisticsSurvey();
+                request.setAttribute("statistics", statistics);
+                requestDispatcher.forward(request, response);
+            }else{
+                requestDispatcher = request.getRequestDispatcher("/views/errorPage4.jsp");
+                requestDispatcher.forward(request, response);
+            }
+        }
 
-
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/result3.jsp");
-        request.setAttribute("statistics", statistics);
-
-        requestDispatcher.forward(request, response);
     }
 }
--- src/main/java/com/retoree/term_project/servlets/StatisticsUsersServeyServlet.java
+++ src/main/java/com/retoree/term_project/servlets/StatisticsUsersServeyServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.retoree.term_project.Bean.Member;
 import com.retoree.term_project.dao.StatisticsWithDB;
 
 import jakarta.servlet.RequestDispatcher;
@@ -19,20 +20,30 @@ public class StatisticsUsersServeyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession httpSession = request.getSession();
-        
+        Member loginUser = (Member)httpSession.getAttribute("loginUser");
         StatisticsWithDB db = new StatisticsWithDB();
 
         ArrayList userNameArr = userNameArr = db.getUsersInfo();
         HashMap<String, String> survey =  db.getUsersServey();
+        RequestDispatcher requestDispatcher = null;
         
-        System.out.println(survey.get("U1Q1"));
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/result2.jsp");
+        if (loginUser == null){
+            requestDispatcher = request.getRequestDispatcher("/views/errorPage3.jsp");
+            requestDispatcher.forward(request, response);
+        }else{
+            if(loginUser.getAuth().equals("YES")){
+                requestDispatcher = request.getRequestDispatcher("/views/result2.jsp");
+                request.setAttribute("loginData", httpSession.getAttribute("loginUser"));
+                request.setAttribute("userNameArr", userNameArr);
+                request.setAttribute("survey", survey);
+                requestDispatcher.forward(request, response);
+            }else{
+                requestDispatcher = request.getRequestDispatcher("/views/errorPage4.jsp");
+                requestDispatcher.forward(request, response);
+            }
+
+        }
         
-        request.setAttribute("loginData", httpSession.getAttribute("loginUser"));
-        request.setAttribute("userNameArr", userNameArr);
-        request.setAttribute("survey", survey);
-
-
-        requestDispatcher.forward(request, response);
+        
     }
 }
